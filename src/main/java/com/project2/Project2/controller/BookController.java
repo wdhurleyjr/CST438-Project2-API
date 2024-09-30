9c446f330dcc5ba80547c9803a7aa0b29ad514d0
William Hurley <wdhurleyjr@gmail.com>
2024-09-29 18:06:29 -0700

    API completed just needs fine tune adjustments.

--- src/main/java/com/project2/Project2/controller/BookController.java
+++ src/main/java/com/project2/Project2/controller/BookController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/books")
+@RequestMapping("/api/books")
 public class BookController {
 
     @Autowired
--- src/test/java/com/project2/Project2/Project2ApplicationTests.java
+++ src/test/java/com/project2/Project2/Project2ApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
-@SpringBootTest(classes = Project2Application.class)
+@SpringBootTest(classes = Application.class)
 public class Project2ApplicationTests {
 
 	@DynamicPropertySource
