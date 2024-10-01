4e5bbba287e69a98c8e625cb2ba28a96b2c006ae
MitPete <PetercovingtonMitchell@gmail.com>
2024-09-30 19:40:57 -0700

    Integrated MongoDB with NYT Books API and created populate endpoint

---
+++ src/main/java/com/project2/Project2/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.project2.Project2.repository;
+
+import com.project2.Project2.model.Book;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface BookRepository extends MongoRepository<Book, String> {
+}
