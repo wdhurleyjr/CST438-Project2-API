f30622230fb862dab3e98b00cc6590f959f0be28
wdhurleyjr <128835969+wdhurleyjr@users.noreply.github.com>
2024-10-03 10:48:07 -0700

    Revert "Integrated MongoDB with NYT Books API and created populate endpoint"

--- src/main/java/com/project2/Project2/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project2.Project2.repository;
-
-import com.project2.Project2.model.Book;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BookRepository extends MongoRepository<Book, String> {
-}
