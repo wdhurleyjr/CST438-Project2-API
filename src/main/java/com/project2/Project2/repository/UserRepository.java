67d0d499e39eb02a8d926efc97e5bd452edc8938
William Hurley <wdhurleyjr@gmail.com>
2024-10-04 13:51:02 -0700

    Created User Repository.

---
+++ src/main/java/com/project2/Project2/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.project2.Project2.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.project2.Project2.model.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+}
