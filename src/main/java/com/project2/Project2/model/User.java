be7cc0ca68903b906d97e67976c6e7396e783039
William Hurley <wdhurleyjr@gmail.com>
2024-10-06 21:58:00 -0700

    Fixed missing package statement.

--- src/main/java/com/project2/Project2/model/User.java
+++ src/main/java/com/project2/Project2/model/User.java
@@ -1,3 +1,5 @@
+package com.project2.Project2.model;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.index.Indexed;
